624f023c8a3ad3ea33a5b955df75085ff41feef6
andrew <arenn@design.upenn.edu>
2022-06-06 20:36:34 +0100

    Local CRS

--- R/pad.R
+++ R/pad.R
@@ -10,12 +10,30 @@ library(tigris)
 ## set the state
 geography <- "30"
 
+# get name
+geog_name <- 
+  tigris::fips_codes %>%
+  filter(state_code == geography) %>%
+  distinct(state_name) %>%
+  pull()
+
+# get local crs
+geog_proj <-
+  crsuggest::crs_sf %>%
+  filter(str_detect(crs_name, "NAD83\\(HARN\\)"),
+         crs_units == "m") %>%
+  filter(str_detect(crs_name, geog_name)) %>%
+  pull(crs_proj4)
+
+# another way...
+# crsuggest::suggest_crs(block_groups(state = geography, cb = TRUE, class = 'sf'))
+
 ## block groups
 blocks <- 
   block_groups(state = geography, cb = TRUE, class = 'sf') %>% 
-  st_transform(3857) %>% 
+  st_transform(geog_proj) %>% 
   transmute(GEOID, 
-            area_total = units::set_units(st_area(geometry), acres))
+            area_total = units::set_units(st_area(geometry), m^2))
 
 plot(st_geometry(blocks))
 
@@ -74,3 +92,4 @@ protections %>%
               values_from = area_protected) %>%
   mutate_at(vars(starts_with("PAD")), funs(case_when(is.na(.) ~ area_total, TRUE ~ .))) %>%
   write_csv("protections_mt.csv")
+
