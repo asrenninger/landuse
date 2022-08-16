17099d1b834f457daaf7a59aefba170336d7f6f4
Alex Nelms <nelmsal@gmail.com>
2022-08-16 15:55:34 -0400

    fixed FIPS and crs functions
    add functions to get FIPS codes and names
    
    corrected get_crs function to take any geography name type

--- R/help.R
+++ R/help.R
@@ -7,51 +7,52 @@ library(tidyverse)
 library(sf)
 library(tigris)
 library(crsuggest)
+library(rgdal)
 
 ## smoothing
 nn_interpolate <- function(data, depth = 5) {
-  
+
   play <-
     data %>%
     mutate(X = st_coordinates(st_centroid(geometry))[, 1],
            Y = st_coordinates(st_centroid(geometry))[, 2]) %>%
-    st_transform(3857) %>% 
+    st_transform(3857) %>%
     arrange(Y, X) %>%
     rownames_to_column() %>%
     mutate(rowname = as.numeric(rowname)) %>%
     select(-X, -Y) %>%
     st_as_sf()
-  
-  crosswalk <- 
-    play %>% 
-    transmute(id = rowname, 
+
+  crosswalk <-
+    play %>%
+    transmute(id = rowname,
               GEOID)
-  
-  frst_degree <- 
-    play %>% 
-    st_touches() %>% 
+
+  frst_degree <-
+    play %>%
+    st_touches() %>%
     tibble() %>%
     rownames_to_column() %>%
     janitor::clean_names() %>%
     unnest(x) %>%
     transmute(row_id = as.numeric(rowname),
               col_id = x)
-  
-  scnd_degree <- frst_degree %>% 
+
+  scnd_degree <- frst_degree %>%
     rename(id = row_id,
            row_id = col_id) %>%
-    left_join(frst_degree) 
-  
+    left_join(frst_degree)
+
   for (i in 1:depth) {
-    
+
     scnd_degree <-
       scnd_degree %>%
       transmute(id = id,
                 row_id = col_id) %>%
       left_join(frst_degree)
-    
+
   }
-  
+
   final <-
     scnd_degree %>%
     transmute(id = id,
@@ -60,32 +61,43 @@ nn_interpolate <- function(data, depth = 5) {
     distinct(rowname, .keep_all = TRUE) %>%
     ungroup() %>%
     left_join(play) %>%
-    st_as_sf() %>% 
+    st_as_sf() %>%
     select(-rowname) %>%
     st_drop_geometry() %>%
     group_by(id) %>%
-    summarise_if(is.numeric, ~mean(.x, na.rm = TRUE)) %>% 
+    summarise_if(is.numeric, ~mean(.x, na.rm = TRUE)) %>%
     ungroup() %>%
     left_join(crosswalk) %>%
     select(-id) %>%
     select(GEOID, everything()) %>%
     rename_if(is.numeric, ~paste0(., "_lag", depth))
-  
+
   return(final)
-  
+
 }
 
-## get CRS
-get_crs <-
-  function(state){
-    
-    # get name
-    geog_name <- 
+# DATAFRAME OF FIPS CODES
+fips_df <-
       tigris::fips_codes %>%
-      filter(state_code == geography) %>%
-      distinct(state) %>%
-      pull()
-    
+      select(state, state_code, state_name) %>%
+      unique() %>%
+      filter(as.numeric(state_code)<60)
+
+# STATE FIPS CODES
+get_state_row = function(geography) fips_df %>%
+    filter(state_name==geography|state_code==geography|state==geography)
+## GET FIPS
+get_fips = function(geography) get_state_row(geography) %>% pull(state_code)
+## GET ABBREVIATED STATE NAME
+get_state_nm = function(geography)  get_state_row(geography) %>% pull(state)
+## GET FULL STATE NAME
+get_state_name = function(geography)  get_state_row(geography) %>% pull(state_name)
+
+# GET CRS
+get_crs <-
+  function(geography){
+    geog_name <- get_state_name(geography)
+
     # get local crs
     geog_proj <-
       crsuggest::crs_sf %>%
@@ -94,10 +106,10 @@ get_crs <-
       filter(str_detect(crs_name, geog_name)) %>%
       slice(1) %>%
       pull(crs_proj4)
-    
     return(geog_proj)
-    
   }
 
-
-
+# GET PROJ
+if (exists("geography", inherits = FALSE)) {
+  geog_proj <- get_crs(geography)
+}
